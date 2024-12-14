d968c05956d0362ec2aaedcb14e7b44e6e8a9b1f
YDYordanov <jordanj2386@gmail.com>
2024-12-14 16:08:23 +0000

    Cosmetic changes

--- models.py
+++ models.py
@@ -8,8 +8,10 @@ import jax.nn as nn  # activation fn-s
 from jax import random, grad, jit, vmap
 
 
-# First, let's implement a simple FFN for images, to learn the JAX basics
 class FFN:
+    """
+    First, let's implement a simple FFN for images, to learn the JAX basics
+    """
     def __init__(self, in_size: int, h_size: int, out_size: int):
         self.in_size = in_size
         self.h_size = h_size
@@ -52,50 +54,19 @@ class FFN:
             out = self.forward(x)
             # Model predictions
             predictions = out.argmax(axis=-1)
-            print(out)
-            print(predictions)
+            #print(out)
+            #print(predictions)
 
 
     def run_epoch(self, x_data, y_data):
         for batch_id, (x, y) in enumerate(zip(x_data, y_data)):
             out = self.forward(x)
-            print('Output:', out)
+            #print('Output:', out)
             loss = self.cross_entropy_loss(out, y)
-            print('Loss:', loss)
+            #print('Loss:', loss)
 
             if batch_id % 10 == 0:
                 # Training evaluation
                 # ToDo: replace this with dev evaluation
                 self.evaluate(x_data, y_data)
             #loss.backward()
-
-
-"""
-# https://github.com/jax-ml/jax/blob/main/README.md
-def predict(params, inputs):
-  for W, b in params:
-    outputs = jnp.dot(inputs, W) + b
-    inputs = jnp.tanh(outputs)  # inputs to the next layer
-  return outputs                # no activation on last layer
-
-
-def loss(params, inputs, targets):
-  preds = predict(params, inputs)
-  return jnp.sum((preds - targets)**2)
-
-
-grad_loss = jit(grad(loss))  # compiled gradient evaluation function
-perex_grads = jit(vmap(grad_loss, in_axes=(None, 0, 0)))  # fast per-example grads
-
-
-# Batched application of "apply_matrix" (https://jax.readthedocs.io/en/latest/quickstart.html)
-@jit
-def vmap_batched_apply_matrix(batched_x):
-  return vmap(apply_matrix)(batched_x)
-
-
-np.testing.assert_allclose(naively_batched_apply_matrix(batched_x),
-                           vmap_batched_apply_matrix(batched_x), atol=1E-4, rtol=1E-4)
-print('Auto-vectorized with vmap')
-%timeit vmap_batched_apply_matrix(batched_x).block_until_ready()
-"""
